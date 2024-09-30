08c280b77d51a768955993284556adc53e254fd3
RutaHeidemane <rutaheidemane@gmail.com>
2024-09-30 13:21:56 +0300

    Add files via upload

---
+++ infene.py
@@ -0,0 +1,15 @@
+
+print('Hellow World!')
+a = 10
+b = 5.0
+
+print ( a+b )
+print ( a-b )
+print ( a/b )
+print ( a*b )
+print ( a**b )
+print ( a//b )
+
+user = input("Enter your name :")
+print(f"Hellow {user}!")
+user2=int(input("enter number:"))
