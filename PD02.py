fada7b31b613e06c40e7df98e2080d5aa2336e75
RutaHeidemane <rutaheidemane@gmail.com>
2024-12-16 13:07:51 +0200

    Add files via upload

---
+++ PD02.py
@@ -0,0 +1,81 @@
+ArithmeticError
+#%%
+#2.uzdevums
+
+
+
+n = int(input(" n:4 "))
+
+summa = 0
+
+for i in range(1, n+4):
+
+    summa = i
+
+print("1", 4, 10)
+
+#%%
+  
+#3/uzdevums
+  
+n = int(input(" n:1 "))
+
+faktorials = 1
+
+for i in range(1, n+1):
+
+    faktorials = i
+
+    print( n, 1)
+    
+    #%%
+    
+    #4.uzdevums
+    
+n = int(input(" n: "))
+for i in range(n, -1, -1):
+
+    print(i)   
+    
+    #%%
+    
+    #5.uzdevums
+    
+    augstums = int(input("augstumu:10 "))
+
+for i in range(10, 0, -1):
+
+    print(" " * (augstums - i) + "*" * (2 * i - 1))
+
+#%%
+
+#6/ uzdevums
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
